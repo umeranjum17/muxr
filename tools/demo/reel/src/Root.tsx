44a1eb89612a83b72eb3d88f41b4068bf1bf3528
umer <umeranjum17@gmail.com>
2026-08-22 18:48:45 +0400

    Revert "Generate store screenshots as product marketing, not documentation"
    This reverts commit 8f4a62f230ae1eb1804b0393d5bb2083891838c8.

--- tools/demo/reel/src/Root.tsx
+++ tools/demo/reel/src/Root.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Composition } from 'remotion';
 import { Film } from './Film';
-import { FPS, H, TOTAL, W, starts } from './config';
-import { PhoneScreen } from './screens';
-
-const Phone: React.FC = () => <PhoneScreen tapAt={starts.approval + 120} />;
+import { FPS, H, TOTAL, W } from './config';
 
 export const Root: React.FC = () => (
-    <>
-        <Composition id="film" component={Film} durationInFrames={TOTAL}
-            fps={FPS} width={W} height={H} />
-        <Composition id="phone" component={Phone} durationInFrames={TOTAL}
-            fps={FPS} width={403} height={896} />
-    </>
+    <Composition id="film" component={Film} durationInFrames={TOTAL}
+        fps={FPS} width={W} height={H} />
 );
